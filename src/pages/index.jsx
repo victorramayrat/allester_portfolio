5fa97dc1c24b1f2699c7cc8e6092e3b9b6cafb17
Victor <victor@marketgrowth.io>
2023-07-06 16:03:15 -0700

    Update favicon and social links

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -26,6 +26,29 @@ function SocialLink({ className, href, children, icon: Icon }) {
   )
 }
 
+function BriefcaseIcon(props) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
+      {...props}
+    >
+      <path
+        d="M2.75 9.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
+        className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
+      />
+      <path
+        d="M3 14.25h6.249c.484 0 .952-.002 1.316.319l.777.682a.996.996 0 0 0 1.316 0l.777-.682c.364-.32.832-.319 1.316-.319H21M8.75 6.5V4.75a2 2 0 0 1 2-2h2.5a2 2 0 0 1 2 2V6.5"
+        className="stroke-zinc-400 dark:stroke-zinc-500"
+      />
+    </svg>
+  )
+}
+
 function MailIcon(props) {
   return (
     <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
@@ -103,12 +126,16 @@ export default function Home() {
               <SocialLink href="https://twitter.com/allester" icon={TwitterIcon} className="mt-4">
                 Connect with me on Twitter
               </SocialLink>
+              <SocialLink href="mailto:allester@ramayrat.com" icon={MailIcon} className="mt-4">
+              Contact me via e-mail
+              </SocialLink>
               <SocialLink
-                href="mailto:allester@ramayrat.com"
-                icon={MailIcon}
+                href="https://docs.google.com/document/d/1kY6UxCejPVzf7jLrRzyUh4bV4sMTeHCWSfrHjc2daes/edit?usp=sharing"
+                icon={BriefcaseIcon}
+                target="_blank"
                 className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
               >
-                allester@ramayrat.com
+                Download my CV
               </SocialLink>
             </ul>
           </div>
