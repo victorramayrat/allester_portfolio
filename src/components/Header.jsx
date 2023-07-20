cd10af29c4a1f4ff75d72d1a3ea6b17aaf7dbe1d
allester <allester@ramayrat.com>
2023-07-20 16:20:35 -0700

    fixed nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ import { useRouter } from 'next/router'
 import { Popover, Transition } from '@headlessui/react'
 import clsx from 'clsx'
 
-import { Container } from '@/components/Container'
 import avatarImage from '@/images/allester-1024.png'
 
 function CloseIcon(props) {
@@ -121,10 +120,10 @@ function MobileNavigation(props) {
             </div>
             <nav className="mt-6">
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
-                <MobileNavItem href="/">About</MobileNavItem>
-                <MobileNavItem href="/projects">Projects</MobileNavItem>
-                <MobileNavItem href="/experience">Experience</MobileNavItem>
-                <MobileNavItem href="/contact">Contact</MobileNavItem>
+                <MobileNavItem href="/#about">About</MobileNavItem>
+                <MobileNavItem href="/#experience">Experience</MobileNavItem>
+                <MobileNavItem href="/#projects">Projects</MobileNavItem>
+                <MobileNavItem href="/#contact">Contact</MobileNavItem>
               </ul>
             </nav>
           </Popover.Panel>
@@ -163,9 +162,9 @@ function DesktopNavigation(props) {
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem href="/#about">About</NavItem>
-        <NavItem href="/projects">Projects</NavItem>
-        <NavItem href="/experience">Experience</NavItem>
-        <NavItem href="/contact">Contact</NavItem>
+        <NavItem href="/#experience">Experience</NavItem>
+        <NavItem href="/#projects">Projects</NavItem>
+        <NavItem href="/#contact">Contact</NavItem>
       </ul>
     </nav>
   )
@@ -228,6 +227,7 @@ function Avatar({ large = false, className, ...props }) {
   return (
     <Link
       href="/"
+      target='_parent'
       aria-label="Home"
       className={clsx(className, 'pointer-events-auto')}
       {...props}
