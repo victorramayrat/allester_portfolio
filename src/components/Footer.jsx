121dda7bd451477b38a011cabe16fa1569266900
allester <allester@ramayrat.com>
2023-07-21 22:42:06 -0700

    added animations

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-
+import { AnimationOnScroll } from 'react-animation-on-scroll'
 import { Container } from '@/components/Container'
 
 function NavLink({ href, children }) {
@@ -15,25 +15,29 @@ function NavLink({ href, children }) {
 
 export function Footer() {
   return (
-    <footer className="mt-32">
-      <Container.Outer>
-        <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
-          <Container.Inner>
-            <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
-              <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-                <NavLink href="/">About</NavLink>
-                <NavLink href="/projects">Projects</NavLink>
-                <NavLink href="/experience">Experience</NavLink>
-                <NavLink href="/contact">Contact</NavLink>
-              </div>
-              <p className="text-sm text-zinc-400 dark:text-zinc-500">
-                &copy; {new Date().getFullYear()} Allester Ramayrat. All rights
-                reserved.
-              </p>
+    <div id='footer'>
+        <AnimationOnScroll animateIn="animate-fade" animateOnce="true">
+        <footer className="mt-32">
+          <Container.Outer>
+            <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
+              <Container.Inner>
+                <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+                  <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
+                    <NavLink href="/">About</NavLink>
+                    <NavLink href="/projects">Projects</NavLink>
+                    <NavLink href="/experience">Experience</NavLink>
+                    <NavLink href="/contact">Contact</NavLink>
+                  </div>
+                  <p className="text-sm text-zinc-400 dark:text-zinc-500">
+                    &copy; {new Date().getFullYear()} Allester Ramayrat. All rights
+                    reserved.
+                  </p>
+                </div>
+              </Container.Inner>
             </div>
-          </Container.Inner>
-        </div>
-      </Container.Outer>
-    </footer>
+          </Container.Outer>
+        </footer>
+      </AnimationOnScroll>
+    </div>
   )
 }
