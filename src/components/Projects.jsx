01969e954f77f314f0974e2b1bc880ed8314aead
allester <allester@ramayrat.com>
2023-07-24 12:38:20 -0700

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -55,8 +55,8 @@ const posts = [
           <div className="py-24 sm:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
               <div className="mx-auto max-w-2xl lg:max-w-4xl">
-                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Selected Projects</h2>
-                <p className="mt-2 text-lg leading-8 text-gray-600">
+                <h2 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">Selected Projects</h2>
+                <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
                   Learn how to grow your business with our expert advice.
                 </p>
                 <div className="mt-8 space-y-20 lg:mt-10 lg:space-y-20">
