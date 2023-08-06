56f39f6387c84b042aedc232c1f4d1c44f06ca3a
Victor <github@ramayrat.com>
2023-08-06 16:10:15 -0700

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-import { Title } from '../components/Title'
-import { About } from '../components/About'
-import { Experience } from '../components/Experience'
-import { Projects } from '../components/Projects'
-import { Contact } from '../components/Contact'
+import { Title } from '@/components/Title'
+import { About } from '@/components/About'
+import { Experience } from '@/components/Experience'
+import { Projects } from '@/components/Projects'
+import { Contact } from '@/components/Contact'
 
 export default function Home() {
   return (
