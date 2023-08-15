8efa359c0c5efc5cfb8e3fc7ffcf985a7c3f9141
Victor <github@ramayrat.com>
2023-08-15 16:39:26 -0700

    Update TitleSection.jsx

--- src/components/TitleSection.jsx
+++ src/components/TitleSection.jsx
@@ -30,7 +30,7 @@ export function Title() {
           </div>
           <div className="animate-fade" style={{"animation-delay": "800ms", "animation-fill-mode": "both"}}>
             <p className="mt-6 text-2xl max-w-screen-sm text-zinc-600 dark:text-zinc-400">
-              <strong>#opentowork</strong> for Analytics Engineering, or related roles; either full-time or freelance. Scroll down to learn more.
+              <strong>#opentowork</strong> for Analytics Engineering, Data Analyst, or SWE; either as FTE or contract. Scroll down to learn more.
             </p>
           </div>
           <div className="mt-6 flex gap-6">
